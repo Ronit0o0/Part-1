c7d915fec7086ee80a86ce291cc29dbbd5ab326a
RONITSLAPTOP\ronbo <rathroni@sheridancollege.ca>
2024-01-18 14:20:24 -0500

    Task 1 missile destroys when collision happens

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 { float speed = 5f;
+    Vector2 direction = new Vector2(10, 0);
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rigidbody = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0); 
+        //transform.Translate(1 * speed * Time.deltaTime, 0, 0); 
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+        }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("missile hit something");
+        Destroy(gameObject);
     }
 }
+
